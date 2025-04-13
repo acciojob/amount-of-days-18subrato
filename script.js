f476b583fde9353a408d56a1e9bc17d0924633a8
Subrato Kumar Verma <138965935+18subrato@users.noreply.github.com>
2025-04-13 13:19:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-function daysOfAYear(n){
-	if(n<1 || n>9999){
-		return 0;
-	}
-
-	if(n%4===0 && n%100!==0 || n%400 === 0){
-		return 366;
-	} else {
-		return 365;
-	}
-	
+function daysOfAYear(year) {
+    // Check if the year is a leap year
+    if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+        return 366; // Leap year has 366 days
+    } else {
+        return 365; // Non-leap year has 365 days
+    }
 }
 
-alert(daysOfAYear(2000))
+// Examples:
+console.log(daysOfAYear(2022)); 
+console.log(daysOfAYear(2024)); 
+console.log(daysOfAYear(1900)); 
+console.log(daysOfAYear(2000)); 

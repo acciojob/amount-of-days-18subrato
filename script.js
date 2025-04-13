4b8e0d27469f82c573a33c0b0f4f0739dd657545
Subrato Kumar Verma <138965935+18subrato@users.noreply.github.com>
2025-04-13 13:15:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//your JS code here. If required.
+function daysOfAYear(n){
+	if(n<1 || n>9999){
+		return 0;
+	}
+
+	if(n%4===0 && n%100!==0 || n%400 === 0){
+		return 366;
+	} else {
+		return 365;
+	}
+	
+}
+
+alert(daysOfAYear(2000))
